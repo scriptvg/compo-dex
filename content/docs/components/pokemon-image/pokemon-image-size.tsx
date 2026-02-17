785d346a31d4b199b2a76cf93d97c54995823cb1
scriptvg <velezalan34@gmail.com>
2026-02-17 16:43:53 -0600

    feat(docs): add size example for Pokemon Image component

---
+++ content/docs/components/pokemon-image/pokemon-image-size.tsx
@@ -0,0 +1,83 @@
+import {
+  PokemonImage,
+  PokemonSprite,
+  PokemonFallback,
+} from "@/components/compo-dex/pokemon-image";
+
+function PokemonImageSize() {
+  return (
+    <div className="flex flex-row gap-4 items-center justify-center">
+      <PokemonImage size="sm">
+        <PokemonSprite
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+          alt="Bulbasaur"
+        />
+        <PokemonFallback className="absolute inset-0 z-10" />
+      </PokemonImage>
+      <PokemonImage size="md">
+        <PokemonSprite
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+          alt="Bulbasaur"
+        />
+        <PokemonFallback className="absolute inset-0 z-10" />
+      </PokemonImage>
+      <PokemonImage size="lg">
+        <PokemonSprite
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+          alt="Bulbasaur"
+        />
+        <PokemonFallback className="absolute inset-0 z-10" />
+      </PokemonImage>
+      <PokemonImage size="xl">
+        <PokemonSprite
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+          alt="Bulbasaur"
+        />
+        <PokemonFallback className="absolute inset-0 z-10" />
+      </PokemonImage>
+    </div>
+  );
+}
+
+export default PokemonImageSize;
+
+export const code = `import {
+  PokemonImage,
+  PokemonSprite,
+  PokemonFallback,
+} from "@/components/compo-dex/pokemon-image";
+
+function PokemonImageSize() {
+  return (
+    <div className="flex flex-row gap-4 items-center justify-center">
+      <PokemonImage size="sm">
+        <PokemonSprite
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+          alt="Bulbasaur"
+        />
+        <PokemonFallback className="absolute inset-0 z-10" />
+      </PokemonImage>
+      <PokemonImage size="md">
+        <PokemonSprite
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+          alt="Bulbasaur"
+        />
+        <PokemonFallback className="absolute inset-0 z-10" />
+      </PokemonImage>
+      <PokemonImage size="lg">
+        <PokemonSprite
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+          alt="Bulbasaur"
+        />
+        <PokemonFallback className="absolute inset-0 z-10" />
+      </PokemonImage>
+      <PokemonImage size="xl">
+        <PokemonSprite
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+          alt="Bulbasaur"
+        />
+        <PokemonFallback className="absolute inset-0 z-10" />
+      </PokemonImage>
+    </div>
+  );
+}`;
