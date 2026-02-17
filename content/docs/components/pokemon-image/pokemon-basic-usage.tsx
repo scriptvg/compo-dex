5e8d02e6b1a1f95bca691fd83278260436ac15a2
scriptvg <velezalan34@gmail.com>
2026-02-17 14:45:02 -0600

    feat: update pokemon-image demos and add basic usage example

---
+++ content/docs/components/pokemon-image/pokemon-basic-usage.tsx
@@ -0,0 +1,75 @@
+import {
+  PokemonImage,
+  PokemonSprite,
+  PokemonFallback,
+} from "@/components/compo-dex/pokemon-image";
+
+function PokemonImageBasicUsage() {
+  const pokemon = {
+    name: "Zacian",
+    sprites: {
+      front_default:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/891.png",
+      other: {
+        "official-artwork": {
+          front_default:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/82.png",
+          front_shiny:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/82.png",
+        },
+      },
+    },
+  };
+
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center">
+      <PokemonImage size="lg">
+        <PokemonSprite
+          src={pokemon?.sprites?.other?.["official-artwork"]?.front_default}
+          alt={pokemon?.name}
+        />
+        <PokemonFallback className="absolute inset-0 z-10" />
+      </PokemonImage>
+    </div>
+  );
+}
+
+export default PokemonImageBasicUsage;
+
+export const code = `
+import {
+  PokemonImage,
+  PokemonSprite,
+  PokemonFallback,
+} from "@/components/compo-dex/pokemon-image";
+
+function PokemonImageBasicUsage() {
+  const pokemon = {
+    name: "Zacian",
+    sprites: {
+      front_default:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/891.png",
+      other: {
+        "official-artwork": {
+          front_default:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/82.png",
+          front_shiny:
+            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/82.png",
+        },
+      },
+    },
+  };
+
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center">
+      <PokemonImage size="lg">
+        <PokemonSprite
+          src={pokemon?.sprites?.other?.["official-artwork"]?.front_default}
+          alt={pokemon?.name}
+        />
+        <PokemonFallback className="absolute inset-0 z-10" />
+      </PokemonImage>
+    </div>
+  );
+}
+`;
--- content/docs/components/pokemon-image/pokemon-image-demo.tsx
+++ content/docs/components/pokemon-image/pokemon-image-demo.tsx
@@ -6,10 +6,13 @@ import {
 
 function PokemonImageDemo() {
   return (
-    <div className="flex items-center justify-center gap-4">
-      <PokemonImage>
-        <PokemonSprite src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
-        <PokemonFallback />
+    <div className="flex flex-col gap-4 items-center justify-center">
+      <PokemonImage size="lg">
+        <PokemonSprite
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+          alt="Bulbasaur"
+        />
+        <PokemonFallback className="absolute inset-0 z-10" />
       </PokemonImage>
     </div>
   );
@@ -28,7 +31,10 @@ function PokemonImageDemo() {
   return (
     <div className="flex items-center justify-center gap-4">
       <PokemonImage>
-        <PokemonSprite src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" />
+        <PokemonSprite
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+          alt="Bulbasaur"
+        />
         <PokemonFallback />
       </PokemonImage>
     </div>
