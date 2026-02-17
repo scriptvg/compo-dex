e4c0e8825a977fbb3588e996bbecf1b84908ecca
scriptvg <velezalan34@gmail.com>
2026-02-17 16:44:04 -0600

    docs: register Pokemon Image in components list

--- content/docs/components/components.ts
+++ content/docs/components/components.ts
@@ -3,6 +3,6 @@ export const components = [
     slug: "pokemon-image",
     url: "/docs/components/pokemon-image",
     title: "Pokemon Image",
-    description: "Pokemon Image component",
+    description: "",
   },
 ];
